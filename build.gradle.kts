5cd6b029139bcde97c582f8d167271c2d2472c71
TonyPepe <tonybear55665566@gmail.com>
2023-03-04 13:01:21 +0800

    import JDA, openai. Test openai api.

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,12 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    // kotlin coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+    // Discord
+    implementation("net.dv8tion:JDA:5.0.0-beta.5")
+    // OpenAI GPT-3
+    implementation("com.theokanning.openai-gpt3-java:service:0.11.0")
 }
 
 tasks.test {
